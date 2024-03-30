3e3369ba8cb9c6fdfc16d44dd178cdd5428c88d6
k0mronbek <153188218+k0mronbek@users.noreply.github.com>
2024-03-30 20:15:15 +0500

    Add files via upload

---
+++ c/strchr.c
@@ -0,0 +1,19 @@
+#include "../h/strchr.h"
+
+// int main()
+// {
+//     const char* str = "Code architecture! :-)";
+
+//     char* result = my_strchr(str, '!');
+
+//     if (result != NULL)
+//     {
+//         printf("Found: %s\n", result);
+//     }
+//     else
+//     {
+//         printf("Not found.\n");
+//     }
+
+//     return 0;
+// }
