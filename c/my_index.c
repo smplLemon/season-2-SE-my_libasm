f0d24b60c2cfc260d79dce96cce8f4d2ba98efdf
Khalid <133192175+Kenjayev2002A@users.noreply.github.com>
2024-03-30 20:08:25 +0500

    Create my_index.c

---
+++ c/my_index.c
@@ -0,0 +1,19 @@
+#include "../h/index.h"
+
+// int main()
+// {
+//     const char* haystack = "Code architecture! ;-)";
+//     int needle = 'a';
+
+//     int index = my_index(haystack, needle);
+
+//     if (index != -1)
+//     {
+//         printf("Index: %d\n", index);
+//     }
+//     else
+//     {
+//         printf("Index not found.\n");
+//     }
+//     return 0;
+// }
